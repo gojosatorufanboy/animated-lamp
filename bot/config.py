8d08255608a818df707fb0da10077e10d73130cc
Gojo <101966680+gojosatorufanboy@users.noreply.github.com>
2024-06-29 18:54:49 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,13 +3,13 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID", "4857766"))
+    API_HASH = os.environ.get("API_HASH", "6c3c6facf5598a4b318e138f8c407028")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7075140249:AAGtuZvVyB9AD4W3eofw_KARChGGRRi6qXI")
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001963446260"))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Rss-br:rss-bro-2008@cluster0.djxuyos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1596559467").split("7172796863")]
     MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
